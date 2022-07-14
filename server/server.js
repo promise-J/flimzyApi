093865c78a212cbebfd4315e04215c1f9b5d4495
Promise Johnson <chiemelapromise30@gmail.com>
2022-07-14 15:49:24 +0100

    Add deployment instructions

--- server/server.js
+++ server/server.js
@@ -15,6 +15,7 @@ app.use(express.json())
 app.use(cors({
     origin: ['https://flimzy-chat-app.herokuapp.com', 'http://localhost:3000', 'https://62d02c5e03120000085b0262--flimzyapp.netlify.app/'],
     credentials: true,
+    methods: ["GET", "POST", "DELETE", "PUT"]
 }))
 
 
